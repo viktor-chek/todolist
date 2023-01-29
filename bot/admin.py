4795e369cb7c9dddd209874be769e4aa59994911
viktor-chek <vitek9500@mail.ru>
2023-01-29 13:28:53 +0300

    added description, docstrings,tests and code fixed by pip8

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from bot.models import TgUser
+
+
+@admin.register(TgUser)
+class TgAdmin(admin.ModelAdmin):
+    search_fields = ["username", "email", "first_name", "last_name"]
+    list_display = ["username", "email", "first_name", "last_name"]
+    list_filter = ["is_staff", "is_active", "is_superuser"]
+    exclude = ["password"]
+    readonly_fields = ["date_joined", "last_login"]
--- core/admin.py
+++ core/admin.py
@@ -4,8 +4,5 @@ from core.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    search_fields = ["username", "email", "first_name", "last_name"]
-    list_display = ["username", "email", "first_name", "last_name"]
-    list_filter = ["is_staff", "is_active", "is_superuser"]
-    exclude = ["password"]
-    readonly_fields = ["date_joined", "last_login"]
+    search_fields = ["tg_username", "user"]
+    list_display = ["tg_username", "verification_code", "tg_chat_id", "user"]
